25bac2f6cae6d98ff49a4f4e4e993327db46afca
Kevin Capotosto <foexzya@yahoo.com>
2018-06-04 17:38:31 -0700

    initial

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> things = new List<object>();
+
+            things.Add(7);
+            things.Add(28);
+            things.Add(-1);
+            things.Add(true);
+            things.Add("chair");
+            foreach(object item in things)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            int sum = 0;
+            foreach(object item in things)
+            {
+                if(item is int) {
+                    int add = (int)item;
+                    sum += add;
+                }
+            }
+            System.Console.WriteLine(sum);
+        }
+    }
+}
